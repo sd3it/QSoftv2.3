04183c840532f90f5b4db301b288c22c9df61a48
koa-it <alexandr.philippon@gmail.com>
2013-12-19 11:11:07 +0100

    Modif de la fenetre d'ajout component

--- src/drawing/FenetreAjoutComposant.java
+++ src/drawing/FenetreAjoutComposant.java
@@ -2,35 +2,31 @@ package drawing;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Toolkit;
 
-import javax.swing.ButtonGroup;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-import javax.swing.JComboBox;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
 
 import drawing.demos.ConstructScreen;
-
 
 /**
  * @author KoA
  * @version 0.1
  * 
- *          FenetreAjoutComposant : Frame pour ajouter un composant à notre JCanvas
- *          Champs Nom du produits + Quantité + choix de la taille de l'image
+ *          FenetreAjoutComposant : Frame pour ajouter un composant à notre
+ *          JCanvas Champs Nom du produits + Quantité + choix de la taille de
+ *          l'image
  */
 
-public class FenetreAjoutComposant  extends JDialog {
+public class FenetreAjoutComposant extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 
-
 	private javax.swing.JLabel jLabel1, jLabel2, jLabel3;
 	private javax.swing.JTextField jTextField1, jTextField2;
+	private javax.swing.JList list;
 	private javax.swing.JButton jButton1;
 	private javax.swing.JLayeredPane jLayeredPane1;
 	private javax.swing.JRadioButton jRb100, jRb30;
@@ -40,9 +36,8 @@ public class FenetreAjoutComposant  extends JDialog {
 			.getScreenSize();
 	protected int hauteur = (int) tailleEcran.getHeight();
 	protected int largeur = (int) tailleEcran.getWidth();
-	
-	private static int style = 100;
 
+	private static int style = 100;
 
 	public FenetreAjoutComposant() {
 
@@ -59,15 +54,16 @@ public class FenetreAjoutComposant  extends JDialog {
 
 	private void initComponents() {
 		jLabel1 = new javax.swing.JLabel();
-		jLabel2 = new JLabel();
-		jLabel3 = new JLabel();
+		jLabel2 = new javax.swing.JLabel();
+		jLabel3 = new javax.swing.JLabel();
 		jTextField1 = new javax.swing.JTextField();
-		jTextField2 = new JTextField();
+		jTextField2 = new javax.swing.JTextField();
+		list = new javax.swing.JList();
 		jButton1 = new javax.swing.JButton();
 		jLayeredPane1 = new javax.swing.JLayeredPane();
-		jRb100 = new JRadioButton();
-		jRb30 = new JRadioButton();
-		sizeGroup = new ButtonGroup();
+		jRb100 = new javax.swing.JRadioButton();
+		jRb30 = new javax.swing.JRadioButton();
+		sizeGroup = new javax.swing.ButtonGroup();
 
 		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -77,34 +73,39 @@ public class FenetreAjoutComposant  extends JDialog {
 
 		jTextField1.setBounds(95, 15, 300, 20);
 		jLayeredPane1.add(jTextField1, javax.swing.JLayeredPane.DEFAULT_LAYER);
-		
+
+		list.setBounds(95, 45, 160, 120);
+		list.setBorder(BorderFactory.createEtchedBorder(BevelBorder.RAISED,
+				new Color(255, 255, 255), null));
+		list.setFocusable(false);
+		jLayeredPane1.add(list, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
 		jLabel2.setText("Quantity : ");
-		jLabel2.setBounds(40, 50, 70, 20);
+		jLabel2.setBounds(280, 50, 70, 20);
 		jLayeredPane1.add(jLabel2, javax.swing.JLayeredPane.DEFAULT_LAYER);
-		
-		jTextField2.setBounds(95, 50, 50, 20);
+
+		jTextField2.setBounds(335, 50, 50, 20);
 		jLayeredPane1.add(jTextField2, javax.swing.JLayeredPane.DEFAULT_LAYER);
-		
+
 		jLabel3.setText("Size : ");
-		jLabel3.setBounds(190, 50, 50, 20);
+		jLabel3.setBounds(280, 95, 50, 20);
 		jLayeredPane1.add(jLabel3, javax.swing.JLayeredPane.DEFAULT_LAYER);
-		
-		jRb100.setBounds(230, 50, 85, 20);
+
+		jRb100.setBounds(310, 95, 85, 20);
 		jRb100.setText("100 x 100");
 		jRb100.setSelected(true);
 		jLayeredPane1.add(jRb100, javax.swing.JLayeredPane.DEFAULT_LAYER);
-		
-		jRb30.setBounds(230, 80, 85, 20);
+
+		jRb30.setBounds(310, 120, 85, 20);
 		jRb30.setText("30 x 30");
 		jLayeredPane1.add(jRb30, javax.swing.JLayeredPane.DEFAULT_LAYER);
-		
+
 		sizeGroup.add(jRb100);
 		sizeGroup.add(jRb30);
-				
+
 		jButton1.setText("Add");
-		jButton1.setBounds(150, 120, 80, 20);
+		jButton1.setBounds(320, 170, 80, 20);
 		jLayeredPane1.add(jButton1, javax.swing.JLayeredPane.DEFAULT_LAYER);
-		
 
 		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
 				getContentPane());
@@ -117,24 +118,25 @@ public class FenetreAjoutComposant  extends JDialog {
 
 		layout.setVerticalGroup(layout.createParallelGroup(
 				javax.swing.GroupLayout.Alignment.LEADING).addComponent(
-				jLayeredPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 150,
+				jLayeredPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200,
 				javax.swing.GroupLayout.PREFERRED_SIZE));
 
 		this.getContentPane().setBackground(Color.white);
 
 		jButton1.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				ConstructScreen.addImgComponant(jTextField1.getText(), jTextField2.getText());
+				ConstructScreen.addImgComponant(jTextField1.getText(),
+						jTextField2.getText());
 				dispose();
 			}
 		});
-		
+
 		jRb100.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				setStyle(100);
 			}
 		});
-		
+
 		jRb30.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				setStyle(30);
@@ -142,12 +144,12 @@ public class FenetreAjoutComposant  extends JDialog {
 		});
 
 	}
-	
+
 	/**
 	 * @param style
 	 *            the style to set
 	 */
-	
+
 	public static void setStyle(int style) {
 		FenetreAjoutComposant.style = style;
 	}
